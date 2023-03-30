51c60eb0c1f4a690e25bc338b72e58a2b515aa6e
profullstackdeveloper <neverdream9106@gmail.com>
2023-03-30 14:30:03 +0900

    initial

---
+++ precache-manifest.6302c863814aabf6510d72d64c49e142.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "662632b96c1fb2de19fe323e51a12996",
+    "url": "/full-stack-K/index.html"
+  },
+  {
+    "revision": "cbb449098a6ff75f657d",
+    "url": "/full-stack-K/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "bb422877afcd2c6deb74",
+    "url": "/full-stack-K/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "d088631f5db7cd162d24",
+    "url": "/full-stack-K/static/css/main.d8809c1e.chunk.css"
+  },
+  {
+    "revision": "e93a4c7dd3ecd13df3ec",
+    "url": "/full-stack-K/static/js/2.0bb910b0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/full-stack-K/static/js/2.0bb910b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbb449098a6ff75f657d",
+    "url": "/full-stack-K/static/js/3.17e7d748.chunk.js"
+  },
+  {
+    "revision": "bb422877afcd2c6deb74",
+    "url": "/full-stack-K/static/js/4.e80c015c.chunk.js"
+  },
+  {
+    "revision": "d088631f5db7cd162d24",
+    "url": "/full-stack-K/static/js/main.13b3a83c.chunk.js"
+  },
+  {
+    "revision": "18d443672708188c96bf",
+    "url": "/full-stack-K/static/js/runtime-main.2c7369e1.js"
+  },
+  {
+    "revision": "72bd0080e7dc6b4cb402583251d3b5b5",
+    "url": "/full-stack-K/static/media/1223965911478317056.72bd0080.jfif"
+  },
+  {
+    "revision": "990973dcc6fd6cedc552c16cf7dcbd59",
+    "url": "/full-stack-K/static/media/1_YrVNR6t_6msHo6Fftyv3Fg.990973dc.jpeg"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/full-stack-K/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "7d37cfd1a7a506105359b81f93468007",
+    "url": "/full-stack-K/static/media/HKUST.7d37cfd1.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/full-stack-K/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/full-stack-K/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/full-stack-K/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/full-stack-K/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "2df23c65389adaf7ecbc6a67e9842643",
+    "url": "/full-stack-K/static/media/download.2df23c65.jfif"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/full-stack-K/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "49317906ed9a7c5dd61fd5f2f40e1741",
+    "url": "/full-stack-K/static/media/icfvdqcvurmitxl8ucn2.49317906.webp"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/full-stack-K/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/full-stack-K/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/full-stack-K/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/full-stack-K/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
